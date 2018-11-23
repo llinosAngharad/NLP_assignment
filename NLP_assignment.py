386f5a9e3617b85c1a33bed8e727e3952be3ff87
Llinos <llins1@hotmail.com>
2018-11-23 09:01:07 +0000

    Update NLP_assignment.py

--- NLP_assignment.py
+++ NLP_assignment.py
@@ -1,10 +1,15 @@
 from os import listdir
 from os.path import isfile, join
 import nltk
+import nltk.data
 
 my_path = "/Users/AppleUser/Documents/uni/comp_sci/Year2/NLP1/assignment/data/training"
 corpus_root = "/Users/AppleUser/nltk_data/corpora/NLP_assignment"
 onlyfiles = [f for f in listdir(my_path) if isfile(join(my_path, f))]
-print(onlyfiles)
+directory = os.fsencode(my_path)
 
-#/Users/AppleUser/nltk_data
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".txt"):
+        print(filename)
+
