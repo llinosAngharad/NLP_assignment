5f308dc2e02e0253ffd0110b3a6712176d49d540
Llinos <llins1@hotmail.com>
2018-12-02 11:50:27 +0000

    can read in text

--- NLP_assignment.py
+++ NLP_assignment.py
@@ -1,15 +1,28 @@
-from os import listdir
-from os.path import isfile, join
-import nltk
-import nltk.data
+import os
 
-my_path = "/Users/AppleUser/Documents/uni/comp_sci/Year2/NLP1/assignment/data/training"
-corpus_root = "/Users/AppleUser/nltk_data/corpora/NLP_assignment"
-onlyfiles = [f for f in listdir(my_path) if isfile(join(my_path, f))]
-directory = os.fsencode(my_path)
+mypath = os.getcwd() + "/data/training/"
+directory = os.fsencode(mypath)
+counter = 0
+txtfiles = []
+training_texts = []
+
+
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    if filename.endswith(".txt"):
-        print(filename)
+    try:
+        if filename.endswith(".txt"):
+            txtfiles.append(str(filename))
+            counter += 1
 
+            text_file = open(mypath + filename, "r", encoding="utf8")
+            text = text_file.readlines()
+            training_texts.append(text)
+            text_file.close()
+
+    except Exception as e:
+        raise e
+        print("No files found here!")
+
+for text in training_texts:
+    
