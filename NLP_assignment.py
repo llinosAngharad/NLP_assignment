4f1495240f20e87881b97770518abdac0c96024e
Llinos <llins1@hotmail.com>
2018-12-04 10:33:10 +0000

    Split body and header
    And made methods

--- NLP_assignment.py
+++ NLP_assignment.py
@@ -1,42 +1,89 @@
 import os
 import re
 
-mypath = os.getcwd() + "/data/training/"
-directory = os.fsencode(mypath)
-counter = 0
+training_path = os.getcwd() + "/data/training/"
+training_directory = os.fsencode(training_path)
+
+test_tagged_path = os.getcwd() + "/data/test/test_tagged"
+test_untagged_path = os.getcwd() + "/data/test/test_untagged"
+
 txtfiles = []
 training_texts = []
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    try:
-        if filename.endswith(".txt"):
-            txtfiles.append(str(filename))
-            counter += 1
 
-            text_file = open(mypath + filename, "r", encoding="utf8")
-            text = text_file.readlines()
-            training_texts.append(text)
-            text_file.close()
+# Reads training data and creates a list of each .txt file's contents
+def read_training_data(path, directory):
+    counter = 0
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        try:
+            if filename.endswith(".txt"):
+                txtfiles.append(str(filename))
+                counter += 1
 
-    except Exception as e:
-        raise e
-        print("No files found here!")
+                text_file = open(path + filename, "r", encoding="utf8")
+                text = text_file.read()
+                training_texts.append(text)
+                text_file.close()
 
-speaker_set = set()
-location_set = set()
+        except Exception as e:
+            raise e
+            print("No files found here!")
 
-speaker_pattern = r"<speaker>(?:Dr|Mr|Ms|Mrs|Prof|Sir|Professor)?\.?\s?([a-zA-Z ]+),?\s?(?:PhD)?<\/speaker>"
-location_pattern = r"<location>([a-zA-Z0-9 ]+)</location>"
-for text in training_texts:
-    for line in text:
-        line = line.lower()
-        speaker = re.findall(speaker_pattern, line, flags=re.DOTALL)
-        location = re.findall(location_pattern, line, flags=re.DOTALL)
-        for person in speaker:
-            if speaker: speaker_set.add(person)
-        for place in location:
-            if place: location_set.add(place)
 
-print(speaker_set)
-print(location_set)
+# Creates a set of 'known' speakers that appear in the tagged emails
+def find_known_speakers():
+    speaker_set = set()
+    speaker_pattern = r"<speaker>(?:Dr|Mr|Ms|Mrs|Prof|Sir|Professor)?\.?\s?([a-zA-Z ]+),?\s?(?:PhD)?<\/speaker>"
+    for text in training_texts:
+        for line in text:
+            line = line.lower()
+            speaker = re.findall(speaker_pattern, line, flags=re.DOTALL)
+            for person in speaker:
+                if speaker: speaker_set.add(person)
+    return speaker_set
+
+# Creates a set of 'known' locations that appear in the tagged emails
+def find_known_locations():
+    location_set = set()
+    location_pattern = r"<location>([a-zA-Z0-9 ]+)</location>"
+    for text in training_texts:
+        for line in text:
+            line = line.lower()
+            location = re.findall(location_pattern, line, flags=re.DOTALL)
+            for place in location:
+                if place: location_set.add(place)
+    return location_set
+
+# Splits texts into header and body
+def split_text(text_list):
+    for text in text_list:
+        text = text.lower()
+
+        i = text.index("abstract:")
+        body = text[i:]
+        header = text[:i]
+
+        new_header = "~~~~~~~~~~~~~~~~~~~~~~\nHEADER\n"
+        new_header += header
+
+        new_body = "\nBODY\n"
+        new_body += body
+
+        new_text = new_header + new_body
+        print(new_text)
+
+        # header_pattern = r"(.*?)abstract:"
+        # header = re.findall(header_pattern, text, flags=re.DOTALL)
+        # if header:
+        #     header = header[0]
+        #     new_header = "~~~~~~~~~~~~~~~~~~~~~~\nHEADER\n"
+        #     new_header+= header
+
+def main():
+    read_training_data(training_path, training_directory)
+    # print(find_known_speakers())
+    # print(find_known_locations())
+    split_text(training_texts)
+
+main()
