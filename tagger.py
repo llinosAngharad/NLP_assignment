d57c1a157bb4fbf7621e148118cf8891cf3f13a1
Llinos <llins1@hotmail.com>
2018-12-27 17:11:19 +0000

    tagging update
    Times, loc and speaker tagging working -> need to print to file now

--- tagger.py
+++ tagger.py
@@ -2,39 +2,42 @@ import re
 from dateutil import parser as time_parser
 
 def tag_all(filename, text, start_time, end_time, location, speaker):
-    print(filename)
-    text = tag_start_time(text, start_time)
-    text = tag_end_time(text, end_time)
+    print(filename, start_time, end_time, location, speaker)
+    text = tag_time(text, start_time, end_time)
     text = tag_location(text, location)
     text = tag_speaker(text, speaker)
     return text
 
-def tag_start_time(text, start_time):
+# Function to tag both the start and end time
+def tag_time(text, start_time, end_time):
+    # Extract all instances of time from the text and add them to a set
     time_pattern = r"((?:\d{1,2}:\d{2})\s?(?:am|pm|AM|PM|a\.m\.|p\.m\.)?)\s?-?\s?((?:\d{1,2}:\d{2})\s?(?:am|pm|AM|PM|a\.m\.|p\.m\.)?)?"
     times = re.findall(time_pattern, text)
     time_set = set()
     for x in times:
         if x[0] != "":
-            time_set.add(x[0])
+            time_set.add(x[0].lower().replace(" ", "").strip("\n"))
         if x[1] != "":
-            time_set.add(x[1])
-            
+            time_set.add(x[1].lower().replace(" ", "").strip("\n"))
+
+    # Tag start_time
     if start_time is not None:
-        start_time = time_parser.parse(start_time).time()
-
+        start_time = time_parser.parse(start_time).time()   # Convert start time to universal datetime
+        # For every time found in the text
+        print(time_set)
         for x in time_set:
-            time = x.lower().replace(" ", "")
-            datetime = time_parser.parse(time).time()
-
+            datetime = time_parser.parse(x).time()   # Convert time found in text to universal datetime
             if datetime == start_time:
-                text = re.sub(x, "<stime>" + x + "</stime>", text)
-    return text
+                text = re.sub(x, "<stime>" + x + "</stime>", text)  # Search and replace start time with tags
 
-def tag_end_time(text, end_time):
+    # Tag end_time, if it exists
     if end_time is not None:
-        text = re.sub(re.escape(end_time), "<etime>"+end_time+"</etime>", text)
-        end_time = end_time.replace(" ", "")
-        text = re.sub(re.escape(end_time), "<etime>" + end_time + "</etime>", text)
+        end_time = time_parser.parse(end_time).time()   # Convert end time to universal datetime
+        for x in time_set:
+            datetime = time_parser.parse(x).time()   # Convert time found in text to universal datetime
+            if datetime == end_time:
+                text = re.sub(x, "<etime>" + x + "</etime>", text)  # Search and replace end time with tags
+
     return text
 
 def tag_location(text, location):
